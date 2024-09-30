9a6ed7bca7c1707c44e33e19baaa8e5fc640dcef
Mizakson <maxwellizakson@gmail.com>
2024-09-30 14:16:48 -0400

    Finish 'Handling bad urls'

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import Profile from './Profile.jsx'
+import ErrorPage from './ErrorPage.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "profile/:name",
